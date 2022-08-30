0e17050731c5803de4685c35cf43993ac2c9729f
Zaryab-Programmer <muhammadzaryabrafique@gmail.com>
2022-08-30 22:29:16 +0500

    reqwest client

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error>{
+    let data = reqwest::Client::new()
+        .get("https://api.pancakeswap.info/api/v2/tokens/0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82")
+        .send()
+        .await?
+        .text()
+        .await?;
+
+    println!("{:#?}", data);
+   
+    Ok(())
 }
