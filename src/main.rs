4d43f2c08a53046349ec247ef974b9969a95de79
Zaryab-Programmer <muhammadzaryabrafique@gmail.com>
2022-08-30 22:43:06 +0500

    text to json

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,28 @@
+use serde::{Serialize, Deserialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Pancakeswap {
+    updated_at: isize,
+    data: Data,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Data {
+    #[serde(rename = "name")]
+    name: String,
+    symbol: String,
+    price: String,
+    #[serde(rename = "price_BNB")]
+    price_bnb: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error>{
-    let data = reqwest::Client::new()
+    let data: Pancakeswap = reqwest::Client::new()
         .get("https://api.pancakeswap.info/api/v2/tokens/0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82")
         .send()
         .await?
-        .text()
+        .json()
         .await?;
 
     println!("{:#?}", data);
